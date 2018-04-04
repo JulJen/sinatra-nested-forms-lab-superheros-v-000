0c71e64568a75a08a74ad35ecbdf5323cb4c64d7
Jul Jen <juliajen.prog@gmail.com>
2018-04-04 04:33:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,9 @@ set :views, Proc.new { File.join(root, "../views/") }
     # @members = params[:team][:members]
 
     @team = Team.new(params[:team])
-    params[:team][:members].each do |info|
+    members = params[:team][:members]
+
+    members.each do |info|
       Hero.new(info)
     end
     @heroes = Hero.all
